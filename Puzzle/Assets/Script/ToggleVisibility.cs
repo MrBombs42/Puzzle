b33b51161e522ecee663e484069d92a0e490554d
Stefano-Bombs <stefano.zanchetta@insanemedia.com.br>
2024-04-07 22:47:01 -0300

    transparency helper

---
+++ Puzzle/Assets/Script/ToggleVisibility.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Assets.Script
+{
+	public class ToggleVisibility : MonoBehaviour
+	{
+		[SerializeField] private PuzzlePiece[] _pieces;
+
+		private bool _isVisible;
+
+		private void Awake()
+		{
+			_pieces = GetComponentsInChildren<PuzzlePiece>(true);
+
+			foreach (var piece in _pieces)
+			{
+				piece.gameObject.SetActive(false);
+				_isVisible = false;
+			}
+		}
+
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.H))
+			{
+				_isVisible = !_isVisible;
+				foreach (var piece in _pieces)
+				{
+					piece.gameObject.SetActive(_isVisible);
+				}
+			}
+		}
+	}
+}
